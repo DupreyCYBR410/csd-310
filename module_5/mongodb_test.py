53c84f7b2b76161743cabc922f772c078fe24f81
DupreyCYBR410 <88586024+DupreyCYBR410@users.noreply.github.com>
2021-08-27 10:07:33 -0400

    Create mongodb_test.py

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,26 @@
+"""
+Title: mongodb_test.py
+Author: Sarah Duprey
+Date: 27 August 2021
+Description: Test Program for connecting to a 
+             MongoDB Atlas cluster
+"""
+
+import pymongo
+from pymongo import MongoClient
+
+# MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.2dju7.mongodb.net/pytech?retryWrites=true&w=majority"
+
+# connect to MongoDB cluster
+client = MongoClient(url)
+
+# connect to pytech database
+db = client.pytech
+
+# show the connection collections
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names)
+
+# show an exit message
+input("\n\n End of program, press and key to exit...")
