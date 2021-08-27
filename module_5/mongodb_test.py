837c51d0fe0a5ffb233d06b29d532d52fcc073fd
DupreyCYBR410 <88586024+DupreyCYBR410@users.noreply.github.com>
2021-08-27 10:19:53 -0400

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -9,19 +9,15 @@ Description: Test program for connecting to a
 import pymongo
 from pymongo import MongoClient
 
-# MongoDB connection string
+
 url = "mongodb+srv://admin:admin@cluster0.2dju7.mongodb.net/pytech?retryWrites=true&w=majority"
 
-# connect to MongoDB cluster
 client = MongoClient(url)
 
-# connect to pytech database
 db = client.pytech
 
-# show the connection collections
 print("\n -- Pytech COllection List --")
 print(db.list_collection_names)
 
-# show an exit message
 input("\n\n End of program, press and key to exit...")
 
